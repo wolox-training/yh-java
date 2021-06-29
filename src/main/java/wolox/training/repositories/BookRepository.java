40f18fbf7c210bdc85da91a0f83e362dd1d9b3f2
YESID FERNANDO HOYOS <yesidfernandohoyos@MacBook-Pro-de-WOLOX.local>
2021-06-29 11:23:17 -0500

    book respository is created

---
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -0,0 +1,10 @@
+package wolox.training.repositories;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import wolox.training.models.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    public Optional<Book> findFirstByAuthor(String author);
+}
