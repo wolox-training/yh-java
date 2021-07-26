54cea01a101c7fd280be2c5d7d270d3863a3b2ec
YESID FERNANDO HOYOS <yesidfernandohoyos@MacBook-Pro-de-WOLOX.local>
2021-07-26 11:15:04 -0500

    rest client is created to get a book from an external api

---
+++ src/main/java/wolox/training/dto/Author.java
@@ -0,0 +1,31 @@
+package wolox.training.dto;
+
+public class Author {
+
+    private String url;
+    private String name;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "url='" + url + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
