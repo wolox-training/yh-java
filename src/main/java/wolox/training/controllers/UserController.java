48790bd189d5f47b8bc9261570196cadd0d31e34
YESID FERNANDO HOYOS <yesidfernandohoyos@MacBook-Pro-de-WOLOX.local>
2021-08-03 11:27:35 -0500

    paging and sorting are added to user controller and repository methods

--- src/main/java/wolox/training/controllers/UserController.java
+++ src/main/java/wolox/training/controllers/UserController.java
@@ -1,6 +1,7 @@
 package wolox.training.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,8 +38,8 @@ public class UserController {
      * @return saved {@link User}s
      */
     @GetMapping
-    public Iterable findAll() {
-        return userRepository.findAll();
+    public Iterable findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 
     /**
--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package wolox.training.repositories;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,12 +13,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     public Optional<User> findFirstByName(String name);
 
-    public List<User> findByBirthdayBetweenAndNameContainingIgnoreCase(LocalDate startDate, LocalDate endDate,
-            String name);
+    public Page<User> findByBirthdayBetweenAndNameContainingIgnoreCase(LocalDate startDate, LocalDate endDate,
+            String name, Pageable pageable);
 
     @Query("SELECT u FROM User u WHERE (cast(:startDate as date) is null or u.birthday >= :startDate) and "
             + "(cast(:endDate as date) is null or u.birthday <= :endDate) and "
             + "(:name is null or lower(u.name) like lower(concat('%', concat(text(:name), '%'))))")
-    public List<User> findByBirthdayBetweenAndNameContainingIgnoreCaseNameParams(
-            @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("name") String name);
+    public Page<User> findByBirthdayBetweenAndNameContainingIgnoreCaseNameParams(
+            @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, @Param("name") String name, Pageable pageable);
 }
